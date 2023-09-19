2a542376195586be113cb1557744d33c477bfb56
0 <a0@0ui-MacBookPro.local>
2023-09-19 22:26:27 +0900

    react route

---
+++ src/pages/Error.jsx
@@ -0,0 +1,14 @@
+import MainNavigation from "../components./MainNavigation";
+
+const ErrorPage = () => {
+  return (
+    <>
+      <MainNavigation />
+      <main className="content">
+        <h1>An error!</h1>
+      </main>
+    </>
+  );
+};
+
+export default ErrorPage;
---
+++ src/pages/ProductDetail.jsx
@@ -0,0 +1,19 @@
+import { Link, useParams } from "react-router-dom";
+
+const ProductDetailPage = () => {
+  const { productId } = useParams();
+
+  return (
+    <>
+      <h1>Product Detail!</h1>
+      <p>{productId}</p>
+      <p>
+        <Link to=".." relative="path">
+          Back
+        </Link>
+      </p>
+    </>
+  );
+};
+
+export default ProductDetailPage;
